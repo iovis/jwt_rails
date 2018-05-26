9f9d8a0281e7a886c792989c1f25cbf96514bb52
David Marchante <davidmarchan@gmail.com>
2018-05-26 19:21:01 +0200

    Show current_user

--- app/controllers/private_controller.rb
+++ app/controllers/private_controller.rb
@@ -2,6 +2,6 @@ class PrivateController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    render json: { data: 'OK' }
+    render json: { user: current_user }
   end
 end
