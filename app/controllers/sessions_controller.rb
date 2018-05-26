d67e006bb30e1542384e24d1ffe0b9dee8bdf4e7
David Marchante <davidmarchan@gmail.com>
2018-05-26 19:20:50 +0200

    Added Session create controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  rescue_from ActionController::ParameterMissing, with: :render_error
+
+  def create
+    resource = User.find_for_database_authentication(email: login_params[:login])
+
+    if resource&.valid_password?(login_params[:password])
+      return render json: {
+        sucess: true,
+        auth_token: JwtService.encode(sub: resource.id)
+      }
+    end
+
+    render_error
+  end
+
+  private
+
+  def render_error
+    render json: { success: false, error: 'Invalid login or password' }, status: :unauthorized
+  end
+
+  def login_params
+    params.require(:user_login).permit(:login, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   get 'private/index'
-
   get 'public/index'
+  post 'users/sign_in', to: 'sessions#create'
 
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
