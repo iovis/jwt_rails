871310e840ede7a749f71f23be4d7a470564d51e
David Marchante <davidmarchan@gmail.com>
2018-05-26 17:40:50 +0200

    Added JwtService

---
+++ app/services/jwt_service.rb
@@ -0,0 +1,12 @@
+class JwtService
+  EXPIRATION_TIME = 10.minutes
+
+  def self.encode(sub:, exp: EXPIRATION_TIME.from_now, **keyword_args)
+    payload = { sub: sub, exp: exp.to_i, **keyword_args }
+    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+  end
+
+  def self.decode(token)
+    JWT.decode(token, Rails.application.secrets.secret_key_base).first
+  end
+end
