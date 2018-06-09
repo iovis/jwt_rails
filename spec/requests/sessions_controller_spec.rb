a9766009513aa9df69610a4fbdc340333b9d2417
David Marchante <davidmarchan@gmail.com>
2018-06-09 20:23:01 +0900

    SessionsController#destroy specs

--- spec/requests/sessions_controller_spec.rb
+++ spec/requests/sessions_controller_spec.rb
@@ -1,4 +1,6 @@
 describe SessionsController, type: :request do
+  let(:user)  { create :user }
+  let(:token) { JwtToken.generate_for(user) }
   let(:invalid_login_response) do
     {
       success: false,
@@ -6,9 +8,8 @@ describe SessionsController, type: :request do
     }.to_json
   end
 
-  describe '/users/sign_in' do
+  describe 'POST /users/sign_in' do
     let(:sign_in) { '/users/sign_in' }
-    let(:user)    { create :user }
 
     context 'with valid credentials' do
       it 'returns the token' do
@@ -54,6 +55,27 @@ describe SessionsController, type: :request do
 
         expect(response).to have_http_status(:unauthorized)
         expect(response.body).to eq invalid_login_response
+      end
+    end
+  end
+
+  describe 'DELETE /users/sign_out' do
+    let(:sign_out) { '/users/sign_out' }
+
+    context 'when logged in' do
+      it 'deletes the token' do
+        delete sign_out, params: {}, headers: token.to_headers
+
+        expect(response).to have_http_status(:success)
+        expect(user.reload.token).to be_nil
+      end
+    end
+
+    context 'when not logged in' do
+      it 'returns an error' do
+        delete sign_out
+
+        expect(response).to have_http_status(:unauthorized)
       end
     end
   end
